e0d919e56a1cc11ae95a353bdce74be8f5fb3961
Jean Carlo Rodriguez Sanchez <j.carlo0123@gmail.com>
2025-01-12 23:55:46 -0500

    feat: se creó una anotación personalizada, RequireJwtVerification, para ejecutar el método de verificación de JWT cuando se llame algún endpoint del controlador que tenga la anotación

---
+++ src/main/java/com/jean/stardew_valley_api/dto/CrearCategoriasRequestDTO.java
@@ -0,0 +1,23 @@
+package com.jean.stardew_valley_api.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "CrearCategoriasRequestDTO", description = "DTO para crear varias categorias")
+public class CrearCategoriasRequestDTO implements Serializable {
+
+    private static final long serialVersionUID = -4822877250683773556L;
+
+    @Schema(description = "Nombres de las categorias", example = "[\"Frutas\", \"Cocina\"]")
+    private List<String> nombres;
+}
